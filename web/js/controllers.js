a0bbc3720aada7b3eb7ff1fee43ea130b22d10bc
Wei Kin Huang <weikin.huang04@gmail.com>
2013-09-10 21:45:14 -0400

    List view for vm-list

--- web/js/controllers.js
+++ web/js/controllers.js
@@ -22,41 +22,59 @@
 		var apiResponse = esxApi.get({
 			moid : "ha-folder-vm"
 		});
-		var vms = {};
-		var vmArr = [];
-		$scope.totalVms = "Loading...";
-		$scope.vmList = vmArr;
-		$scope.vmOrder = '-id';
 
-		apiResponse.then(function(data) {
-			if (!data.childEntity || !data.childEntity.ManagedObjectReference) {
-				$scope.totalVms = "Error!";
-			} else {
-				$scope.totalVms = data.childEntity.ManagedObjectReference.length;
-			}
-			return data;
-		});
+		var timer = null;
 
-		apiResponse.then(function(data) {
-			if (!data.childEntity || !data.childEntity.ManagedObjectReference) {
+		function loadVms() {
+			var vms = {};
+			var vmArr = [];
+			$scope.totalVms = "Loading...";
+			$scope.vmList = vmArr;
+
+			apiResponse.then(function(data) {
+				if (!data.childEntity || !data.childEntity.ManagedObjectReference) {
+					$scope.totalVms = "Error!";
+				} else {
+					$scope.totalVms = data.childEntity.ManagedObjectReference.length;
+				}
 				return data;
-			}
-			angular.forEach(data.childEntity.ManagedObjectReference, function(vm) {
-				var vmId = vm["#text"];
-				var vmData = {
-					id : vmId,
-					info : esxApi.get({
-						moid : vmId
-					}).then(function(data) {
-						console.log(data);
-						return data;
-					})
-				};
-				vms[vmId] = vmData;
-				vmArr.push(vmData);
 			});
-			console.log(data);
-			return data;
+
+			apiResponse.then(function(data) {
+				if (!data.childEntity || !data.childEntity.ManagedObjectReference) {
+					return data;
+				}
+				angular.forEach(data.childEntity.ManagedObjectReference, function(vm) {
+					var vmId = vm["#text"];
+					var vmData = {
+						id : vmId,
+						info : esxApi.get({
+							moid : vmId
+						}).then(function(data) {
+							console.log(data);
+							return data;
+						})
+					};
+					vms[vmId] = vmData;
+					vmArr.push(vmData);
+				});
+				return data;
+			});
+
+			apiResponse.then(function(data) {
+				setTimeout(function() {
+					loadVms();
+				}, 30000);
+				return data;
+			});
+		}
+		loadVms();
+
+
+		$scope.$on("$destroy", function() {
+			if (timer) {
+				clearInterval(timer);
+			}
 		});
 
 		$scope.powerOn = function(vm) {
@@ -66,21 +84,6 @@
 			esxApi.post({
 				moid : vm.id,
 				method : "powerOn"
-			}).then(function(data) {
-				console.log(data);
-				return data;
-			});
-		};
-		$scope.powerOff = function(vm) {
-			if (!confirm("Power off virtual machine?")) {
-				return;
-			}
-			esxApi.post({
-				moid : vm.id,
-				method : "powerOff"
-			}).then(function(data) {
-				console.log(data);
-				return data;
 			});
 		};
 		$scope.shutdownGuest = function(vm) {
@@ -90,15 +93,11 @@
 			esxApi.post({
 				moid : vm.id,
 				method : "shutdownGuest"
-			}).then(function(data) {
-				console.log(data);
-				return data;
 			});
 		};
 	} ]);
 
 	module.controller("VmController", [ "$scope", "$routeParams", "esxApi", function($scope, $routeParams, esxApi) {
-		var vms = {};
 		$scope.id = $routeParams.id;
 
 		$scope.info = esxApi.get({
